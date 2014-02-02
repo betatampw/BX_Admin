797e437cea065a9544ab779e616a368269275f9d
betatampw <betatampw@gmail.com>
2014-02-02 15:46:43 +0400

    start

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('BX admin app');
+});
+
+app.get('/bitrix/admin/mobile/', function(request, response) {
+  response.send('{"status":"success","target":"9ce2d2410a2bba236918b9cd0aaebf51","sessid_md5":"5df8257808c1b46e49250ce5134edc7b","appmap":{"main":{"url":"\/bitrix\/admin\/mobile\/sale_orders_list.php"},"menu":{"url":"\/bitrix\/admin\/mobile\/left.php"}}}');
 });
 
 var port = process.env.PORT || 5000;
